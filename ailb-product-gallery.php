a7713e7fe2c0774cfd5a0b8fe8f82153393df5de
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2025-03-11 12:45:04 +0100

    ailb prefix problem

--- ailb-product-gallery.php
+++ ailb-product-gallery.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       AILB Product Gallery
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       Not another product gallery block.
  * Version:           0.1.0
  * Requires at least: 6.7
  * Requires PHP:      7.4
